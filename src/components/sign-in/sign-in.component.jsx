22cee0b927bd6ca082c16cdd9ec9a95df59d172d
Mario Jackson <mario@jackson.software>
2020-01-15 21:20:28 +0100

    Created and applied form input component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import FormInput from '../form-input/form-input.component';
 
 import './sign-in.styles.scss';
 
@@ -14,13 +15,11 @@ class SignIn extends React.Component {
 
   handleSubmit = event => {
     event.preventDefault();
-
     this.setState({ email: '', password: '' });
   };
 
   handleChange = event => {
     const { value, name } = event.target;
-
     this.setState({ [name]: value });
   };
 
@@ -30,24 +29,24 @@ class SignIn extends React.Component {
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
-        <form>
-          <input
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
             name='email'
             type='email'
+            label='email'
             value={this.state.email}
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
             required
           />
-          <label>Email</label>
 
-          <input
+          <FormInput
             name='password'
             type='password'
+            label='password'
             value={this.state.email}
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
             required
           />
-          <label>Password</label>
 
           <input type='submit' value='Submit Form' />
         </form>
