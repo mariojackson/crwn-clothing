e6c176ef3a92124c13c748b085b5003a7e418ec2
Mario Jackson <mario@jackson.software>
2020-01-15 20:01:40 +0100

    Created initial sign in and sign up component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 import './App.css';
 
@@ -11,10 +12,10 @@ function App() {
   return (
     <div>
       <Header />
-      <h1>CRWN CLOTHING</h1>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
--- src/pages/homepage.component.jsx
+++ src/pages/homepage.component.jsx
@@ -5,6 +5,7 @@ import Directory from '../components/directory/directory.component';
 
 const HomePage = () => (
   <div className='homepage'>
+    <h1>CRWN CLOTHING</h1>
     <Directory />
   </div>
 );
